5a5060dd052ab7c412bed0a21b72a3b173b863a2
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2026-04-11 18:17:32 +0300

    login

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -1,7 +1,35 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
+import { Info, ShoppingBag, ShoppingCart, Star } from "lucide-react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <div>
-      {children}
-    </div>
+    <>
+      <header>
+        <div className="container flex justify-between items-center text-primary py-4 text-sm">
+          <h1 className="text-3xl font-bold text-primary">SOOQ</h1>
+        
+          <div className="flex items-center gap-2">
+            <Info size={16} />
+
+            <div>تواصل معنا</div>
+          </div>
+        </div>
+      </header>
+
+      <div className="flex flex-col items-center justify-center relative">
+        <div className="absolute bottom-4 left-1/4 -rotate-12">
+          <ShoppingCart size={100} className="text-gray-400 opacity-50" />
+        </div>
+
+        <div className="absolute -top-4 right-1/4 rotate-12">
+          <Star size={85} className="text-gray-400 opacity-50" />
+        </div>
+
+        {children}
+      </div>
+    </>
   )
 }
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,12 +2,19 @@ import { Noto_Sans_Arabic } from "next/font/google"
 
 import "@workspace/ui/globals.css"
 import "@/styles/style.scss"
+import { Metadata } from "next";
 
 
 const fontSans = Noto_Sans_Arabic({
   subsets: ["arabic"],
   variable: "--font-sans",
 })
+
+export const metadata: Metadata = {
+  title: 'SOOQ - المتجر الإلكتروني',
+  description: 'SOOQ - المتجر الإلكتروني',
+};
+
 
 export default function RootLayout({
   children,
