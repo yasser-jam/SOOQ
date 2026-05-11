f258a27de8e6e7e09e04dd7167c3831c9caf96ba
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2026-05-12 00:17:30 +0300

    build

--- apps/web/modules/product/collection/actions.ts
+++ apps/web/modules/product/collection/actions.ts
@@ -10,7 +10,7 @@ import type {
 	ProductCollection,
 	ProductCollectionApiModel,
 	UpdateProductCollectionInput,
-} from './types'
+} from "./types"
 
 const normalizeProductCollection = (
 	collection: ProductCollectionApiModel
