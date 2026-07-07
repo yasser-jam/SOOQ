97a8339fa3da48732b84e831b67da462bef5f50d
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2026-07-07 23:06:43 +0300

    fix install

--- packages/editor-packages/plugin-emotion-cache/tsup.config.ts
+++ packages/editor-packages/plugin-emotion-cache/tsup.config.ts
@@ -1,4 +1,11 @@
 import { defineConfig } from "tsup";
 import tsupconfig from "../tsup-config";
 
-export default defineConfig(tsupconfig);
+export default defineConfig({
+  ...tsupconfig,
+  external: [
+    ...(Array.isArray(tsupconfig.external) ? tsupconfig.external : []),
+    "@emotion/cache",
+    "@emotion/react",
+  ],
+});
