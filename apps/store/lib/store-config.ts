efffec5658ca161de2cdb055b498265e6747a3b2
Yasser-Jamal-Al-Deen <yasserjamalaldeen@gmail.com>
2026-08-18 09:07:08 +0300

    fix

--- apps/store/lib/store-config.ts
+++ apps/store/lib/store-config.ts
@@ -2,7 +2,7 @@ export const STORE_FIXED_THEME_ID = "test" as const
 export const STORE_HOME_PATH = "/"
 
 export const MARKETING_SITE_URL =
-	process.env.NEXT_PUBLIC_MARKETING_URL ?? "https://sooq-web.vercel.app/"
+	process.env.NEXT_PUBLIC_MARKETING_URL ?? "https://ertqaa.vercel.app/"
 
 export const TENANT_UUID_REGEX =
 	/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i
